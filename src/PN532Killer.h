bb1a8e591a09052c87cd3f695c2ca6dea657d59f
whywilson <m.tools@qq.com>
2025-05-27 08:58:36 +0800

    Add new sniffing mode and data upload function, update related command definitions.

--- src/PN532Killer.h
+++ src/PN532Killer.h
@@ -8,10 +8,10 @@
 #ifndef PN532KILLER_H
 #define PN532KILLER_H
 
-#include "PN532_UART.h"
 #include "PN532Cmd.h"
 #include "PN532KillerCmd.h"
 #include "PN532KillerTagTechnology.h"
+#include "PN532_UART.h"
 
 class PN532Killer : public PN532_UART {
 public:
@@ -24,14 +24,21 @@ public:
     bool switchEmulatorEm4100Slot(uint8_t slot);
     bool setMfkey32v2Sniffing();
     bool setMfkey64Sniffing();
+    bool setSnifferMode(uint8_t type);
+    bool setSnifferSlotBlock0(const std::vector<uint8_t> &block0);
+    bool setSnifferUid(const std::string &uid_hex);
 
     PN532KillerTagTechnology::Iso15693TagInfo hf15Scan();
     PN532KillerTagTechnology::Em4100TagInfo lfEm4100Scan();
 
     int8_t tagIndex = -1;
+
 private:
     bool setWorkMode(uint8_t mode, uint8_t type, uint8_t index);
     PN532KillerTagTechnology tagTechnology = PN532KillerTagTechnology();
+
+    bool uploadData(uint8_t type, uint8_t slot, uint16_t index, const std::vector<uint8_t> &datas);
+    bool uploadDataDone(uint8_t type, uint8_t slot, const std::vector<uint8_t> &extra);
 };
 
 #endif
--- src/PN532KillerCmd.h
+++ src/PN532KillerCmd.h
@@ -6,11 +6,17 @@
 namespace PN532KillerCmd {
 using namespace PN532Cmd;
 
+static constexpr uint8_t GetEmulatorData = 0x1C;
+static constexpr uint8_t SetEmulatorData = 0x1E;
+static constexpr uint8_t CheckPn532Killer = 0xAA;
 static constexpr uint8_t SetWorkMode = 0xAC;
+static constexpr uint8_t GetSnifferLog = 0x20;
+static constexpr uint8_t ClearSnifferLog = 0x22;
 
 enum WorkMode { Reader = 0x01, Emulator = 0x02, Sniffer = 0x03 };
 enum TagType { MFC1K = 0x01, ISO15693 = 0x03, EM4100 = 0x04 };
 enum ReaderProtocol { HF_ISO14443A = 0x01, HF_ISO15693 = 0x02, LF_EM4100 = 0x03 };
+enum SnifferType { MFKey32v2 = 0x00, MFKey64 = 0x01 };
 } // namespace PN532KillerCmd
 
 #endif // PN532_KILLER_COMMAND_H
